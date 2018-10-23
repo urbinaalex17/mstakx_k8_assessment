b1ae46f9972df3dd0f23718dbb6fe9a348e85327
urbinaalex17 <w.alexanderuc@gmail.com>
2018-10-22 18:45:19 -0600

    Adding scripts and templates

--- scripts/create_cluster.sh
+++ scripts/create_cluster.sh
@@ -21,6 +21,9 @@ cp -f templates/policy_template.json templates/policy.json
 sed -i -e "s/w.alexanderuc@gmail.com/$ACCOUNT/g" templates/policy.json
 sed -i -e "s/304966747568/$PROJECT_NUMBER/g" templates/policy.json
 
+#Applying the policy to the project
+gcloud projects set-iam-policy $PROJECT_ID policy.json --quiet
+
 echo "Creating a GKE cluster"
 CLUSTER_NAME=mstakx
 gcloud container clusters create $CLUSTER_NAME
