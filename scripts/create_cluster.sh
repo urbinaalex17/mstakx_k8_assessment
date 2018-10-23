759f1c0c7fc3e6501e71013b22c61827edded38f
urbinaalex17 <w.alexanderuc@gmail.com>
2018-10-22 18:53:50 -0600

    Adding scripts and templates

--- scripts/create_cluster.sh
+++ scripts/create_cluster.sh
@@ -17,9 +17,9 @@ export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format text | gre
 export ACCOUNT=$(gcloud config get-value account -q)
 
 #Making a copy of policy_template to replace default parameters
-cp -f templates/policy_template.json templates/policy.json
-sed -i -e "s/w.alexanderuc@gmail.com/$ACCOUNT/g" templates/policy.json
-sed -i -e "s/304966747568/$PROJECT_NUMBER/g" templates/policy.json
+cp -f $PWD/../templates/policy_template.json $PWD/../templates/policy.json
+sed -i -e "s/w.alexanderuc@gmail.com/$ACCOUNT/g" $PWD/../templates/policy.json
+sed -i -e "s/304966747568/$PROJECT_NUMBER/g" $PWD/../templates/policy.json
 
 #Applying the policy to the project
 gcloud projects set-iam-policy $PROJECT_ID policy.json --quiet
