2db3a49a180ae247287e7b30467db493b15f2049
urbinaalex17 <w.alexanderuc@gmail.com>
2018-10-23 22:12:35 -0600

    Improving deploy-guest-book.sh script

--- scripts/deploy-guest-book.sh
+++ scripts/deploy-guest-book.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 YAML_DIR="../yaml"
 STAGING_DIR=$YAML_DIR/staging
-STAGING_REDIS_MASTER_FILE=$STAGING_DIR/redis-master-deployment-staging.yaml
+PRODUCTION_DIR=$YAML_DIR/production
+STAGING_FILE=$STAGING_DIR/guestbook-staging.yaml
+PRODUCTION_FILE=$PRODUCTION_DIR/guestbook-production.yaml
 
-kubectl apply -f $STAGING_REDIS_MASTER_FILE
+echo -e "\nDeploying Guest-book application on staging namespace"
+kubectl apply -f $STAGING_FILE
 
+echo -e "\nDeploying Guest-book application on production namespace"
+kubectl apply -f $PRODUCTION_FILE
+
+echo -e "\nGathering information about the environments"
+
+INGRESS_CONTROLLER_IP=$(kubectl -n ingress-nginx get svc | awk {'print $4'} | grep [0-9])
+
+echo -e "\nUpdate your host file as follow:"
+echo -e "\nFor Production:"
+echo "$INGRESS_CONTROLLER_IP guestbook.mstakx.io"
+
+echo -e "\nFor Staging:"
+echo "$INGRESS_CONTROLLER_IP staging-guestbook.mstakx.io"
