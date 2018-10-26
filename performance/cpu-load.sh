d69a83665ceb599cfc17301ac3373537e4b5f045
urbinaalex17 <w.alexanderuc@gmail.com>
2018-10-25 19:15:39 -0600

    CPU Load test

--- performance/cpu-load.sh
+++ performance/cpu-load.sh
@@ -10,3 +10,26 @@
 
 LOAD="dd if=/dev/zero of=/dev/null bs=100M count=1000"
 
+echo -e "\n\nGathering information about Horizontal Pod Autoscalers"
+sleep 15
+
+echo -e "Horizontal Pod Autoscaler for Staging namespace"
+kubectl -n staging get hpa
+
+echo -e "Horizontal Pod Autoscaler for Production namespace"
+kubectl -n production get hpa
+
+
+echo -e "\nCPU Load test"
+echo -e "In order to perform this CPU load test, please open up 3 terminal sessions in your Google Cloud Shell Environment and execute \
+         the following command in each terminal session"
+FRONTS=$(kubectl -n production get pods -l app=guestbook | grep front | awk {'print $1'})
+for frontend in $FRONTS
+do
+  i=1
+  echo -e "\nExecute this in Terminal $i"
+  echo -e "kubectl -n production exec -ti $frontend -- \$LOAD"
+  sleep 10
+done
+
+#kubectl -n production get pods -l app=guestbook
